8fd3b3a63be0f371e19c885b9ac2a641e6153f6d
Chris Jeppesen <jeppesen@lasp.colorado.edu>
2021-02-05 15:31:09 -0700

    Using new PositionVector and DirectionVector classes

--- PerspectiveCamera.h
+++ PerspectiveCamera.h
@@ -11,28 +11,38 @@ namespace kwantrace {
   template<typename pixtype=uint8_t>
   class PerspectiveCamera : public Camera<pixtype> {
   public:
-    Eigen::Vector3d direction;
-    Eigen::Vector3d right;
-    Eigen::Vector3d up;
+    DirectionVector direction;
+    DirectionVector right;
+    DirectionVector up;
+    static double angle2dir(double angle, double rightlen) {
+      //http://www.povray.org/documentation/3.7.0/r3_4.html#r3_4_2 from direction_length in default perspective camera
+      return 0.5 * rightlen / tand(angle/2);
+    }
+    static double dir2angle(double dirlen, double rightlen) {
+      //dirlen=0.5 * rightlen / tand(angle/2)
+      //2*dirlen=rightlen / tand(angle/2)
+      // tand(angle/2)=rightlen /(2*dirlen)
+      //      angle/2 =atand(rightlen /(2*dirlen))
+      //      angle   =2*atand(rightlen /(2*dirlen))
+      return 2*atand(rightlen /(2*dirlen));
+    }
+    PerspectiveCamera() : direction(PositionVector(1, 0, 0)), right(PositionVector(0, 1, 0)),
+                          up(PositionVector(0, 0, 1)) {};
 
-    PerspectiveCamera() : direction(Eigen::Vector3d(1, 0, 0)), right(Eigen::Vector3d(0, 1, 0)),
-                          up(Eigen::Vector3d(0, 0, 1)) {};
+    PerspectiveCamera(double width, double height) : direction(PositionVector(1, 0, 0)),
+                                                     right(PositionVector(0, width / height, 0)),
+                                                     up(PositionVector(0, 0, 1)) {};
 
-    PerspectiveCamera(double width, double height) : direction(Eigen::Vector3d(1, 0, 0)),
-                                                     right(Eigen::Vector3d(0, width / height, 0)),
-                                                     up(Eigen::Vector3d(0, 0, 1)) {};
+    PerspectiveCamera(double width, double height, double angle) : direction(PositionVector(angle2dir(angle, width / height), 0, 0)),
+                                                                   right(PositionVector(0, width / height, 0)),
+                                                                   up(PositionVector(0, 0, 1)) {};
 
-    PerspectiveCamera(double width, double height, double angle) : direction(
-            Eigen::Vector3d(0.5 * (width / height) / tan(deg2rad(angle) / 2), 0, 0)),
-                                                                   right(Eigen::Vector3d(0, width / height, 0)),
-                                                                   up(Eigen::Vector3d(0, 0, 1)) {};
-
-    PerspectiveCamera(Eigen::Vector3d &Ldirection, Eigen::Vector3d &Lright, Eigen::Vector3d &Lup) : direction(
+    PerspectiveCamera(PositionVector &Ldirection, PositionVector &Lright, PositionVector &Lup) : direction(
             Ldirection), right(Lright), up(Lup) {};
   protected:
     virtual Ray project_local(double x, double y) {
       Ray result;
-      result.v = direction + right * x + up * y;
+      result.v = static_cast<DirectionVector>(direction + right * x + up * y);
       return result;
     }
   };
