c0473caf0791deefab1735c3e85b17dac8d347cb
Chris Jeppesen <jeppesen@lasp.colorado.edu>
2021-02-05 15:29:32 -0700

    Using new PositionVector and DirectionVector classes

--- Field.h
+++ Field.h
@@ -12,31 +12,27 @@ namespace kwantrace {
   template<int N, typename T=double>
   class Field {
   private:
-    typedef Eigen::Vector3d InVector;
     typedef Eigen::Matrix<T,N,1> OutVector;
   protected:
-    virtual OutVector field_local(const InVector& r)=0;
+    virtual OutVector field_local(const PositionVector& r)=0;
   public:
     //! Transformation chain
     TransformChain transformChain;
     void prepareRender() {
       transformChain.prepareRender();
     }
-    virtual OutVector operator()(const InVector& r) {return field_local(mul1(transformChain.Mw2b, r));};
+    virtual OutVector operator()(const PositionVector& r) {return field_local(transformChain.Mw2b*r);};
     virtual ~Field() {};
-    Eigen::Matrix<T,N,1> operator()(double x, double y, double z) {return *this(InVector(x,y,z));};
+    Eigen::Matrix<T,N,1> operator()(double x, double y, double z) {return *this(PositionVector(x, y, z));};
   };
-  typedef Field<5,double> ColorField;
 
   class ConstantColor: public ColorField {
   private:
-    typedef Eigen::Vector3d InVector;
-    typedef Eigen::Matrix<double,5,1> OutVector;
-    OutVector value;
-    virtual OutVector field_local(const InVector& r) override {return value;}
+    ObjectColor value;
+    virtual ObjectColor field_local(const PositionVector& r) override {return value;}
   public:
-    ConstantColor(OutVector Lvalue):value(Lvalue) {};
-    ConstantColor(double r, double g, double b, double f, double t) {value<< r,g,b,f,t;};
+    ConstantColor(ObjectColor Lvalue):value(Lvalue) {};
+    ConstantColor(double r=0, double g=0, double b=0, double f=0, double t=0) {value<< r,g,b,f,t;};
   };
 }
 
