147417f19e74f7fc848d16540e6e0391b0dd8174
Chris Jeppesen <jeppesen@lasp.colorado.edu>
2021-02-11 10:15:06 -0700

    Make the camera right-handed. I'm still not totally happy with it.

--- PerspectiveCamera.h
+++ PerspectiveCamera.h
@@ -14,9 +14,9 @@ namespace kwantrace {
    */
   class PerspectiveCamera : public Camera {
   public:
-    Direction direction;
     Direction right;
     Direction up;
+    Direction direction;
     static double angle2dir(double angle, double rightlen) {
       //http://www.povray.org/documentation/3.7.0/r3_4.html#r3_4_2 from direction_length in default perspective camera
       return 0.5 * rightlen / tand(angle/2);
@@ -29,19 +29,31 @@ namespace kwantrace {
       //      angle   =2*atand(rightlen /(2*dirlen))
       return 2*atand(rightlen /(2*dirlen));
     }
-    PerspectiveCamera() : direction(Position(1, 0, 0)), right(Position(0, 1, 0)),
-                          up(Position(0, 0, 1)) {};
+    PerspectiveCamera():
+      right    (Direction(1,0,0)),
+      up       (Direction(0,0,1)),
+      direction(Direction(0,1,0)) {
+    };
 
-    PerspectiveCamera(double width, double height) : direction(Position(1, 0, 0)),
-                                                     right(Position(0, width / height, 0)),
-                                                     up(Position(0, 0, 1)) {};
+    PerspectiveCamera(double width, double height):
+      right    (Direction(width / height, 0,0)),
+      up       (Direction(0,0,1)),
+      direction(Direction(0,1,0)) {
 
-    PerspectiveCamera(double width, double height, double angle) : direction(Position(angle2dir(angle, width / height), 0, 0)),
-                                                                   right(Position(0, width / height, 0)),
-                                                                   up(Position(0, 0, 1)) {};
+    };
 
-    PerspectiveCamera(Position &Ldirection, Position &Lright, Position &Lup) : direction(
-            Ldirection), right(Lright), up(Lup) {};
+    PerspectiveCamera(double width, double height, double angle):
+      right    (Direction(width / height, 0,0)),
+      up       (Direction(0, 0, 1)),
+      direction(Direction(0,angle2dir(angle, width / height), 0)) {
+    };
+
+    PerspectiveCamera(Direction &Lright, Direction &Lup,Direction &Ldirection):
+      right(Lright),
+      up(Lup),
+      direction(Ldirection) {
+
+    };
   protected:
     virtual Ray project_local(double x, double y) const override {
       Ray result;
